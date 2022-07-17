092e9bbe616bf33ea9a7c78115ef751b8a7a8fd9
kadeksatya <kadeksatya0399@gmail.com>
2022-07-17 17:09:03 +0800

    fix

--- app/Http/Controllers/API/RatingController.php
+++ app/Http/Controllers/API/RatingController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\HasRatings;
 use App\Http\Controllers\Controller;
-use App\Product;
 use App\Rating;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
